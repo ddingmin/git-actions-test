ed6804c164aadfa4679fb47eaebdd301e3e7b83d
ddingmin <ddingminyt@gmail.com>
2023-08-12 18:38:56 +0900

    init

---
+++ git-actions-test/src/main/java/com/example/gitactionstest/GitActionsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionsTestApplication.class, args);
+	}
+
+}
